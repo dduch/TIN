4c8d133b6bfcebea53b5ac30a66f26d0f4e8054b
vitalik <vx0@yandex.ru>
2015-04-30 18:15:49 +0200

    Protokol wstepny pomysl

--- tin/src/ProtocolHandler.cpp
+++ tin/src/ProtocolHandler.cpp
@@ -1,9 +1,31 @@
 #include "ProtocolHandler.h"
+#include<iostream>
+#include<string.h>
 
-ProtocolHandler:: ProtocolHandler(){
+ProtocolHandler:: ProtocolHandler(){}
 
+ProtocolHandler:: ~ProtocolHandler(){}
+
+struct ProtocolPacket;
+
+struct ProtocolPacket ProtocolHandler::prepareDatagramACK(unsigned int ack_number)
+{
+	struct ProtocolPacket ack_packet;
+	//ack_number tylko do 20 bitow, inaczej WTF
+	ack_packet.type = (ack_number < 1048576)? ACK : WTF ;
+	ack_packet.number = ack_number;
+	return ack_packet;
 }
 
-ProtocolHandler:: ~ProtocolHandler(){
-
+struct ProtocolPacket ProtocolHandler::prepareDatagramRQ(unsigned int rq_str_length, const char* filename, unsigned int size_of_filename)
+{
+	struct ProtocolPacket rq_packet;
+	rq_packet.filename = new char[size_of_filename];
+	strncpy(rq_packet.filename, filename, size_of_filename);
+	rq_packet.filename[size_of_filename - 1] = '\0';
+	//rq_str_length tylko do 8 bitow, inaczej WTF
+	rq_packet.type = (rq_str_length < 256)? RQ : WTF ;
+	rq_packet.str_length = rq_str_length;
+	return rq_packet;
 }
+
--- tin/src/ProtocolHandler.h
+++ tin/src/ProtocolHandler.h
@@ -1,11 +1,32 @@
 #ifndef PROTOCOLHANDLER_H_
 #define PROTOCOLHANDLER_H_
 
+//Rodzaje pakietow jak w dokumentacji
+#define DATA 		0x00
+#define ACK 		0x01
+#define RQ 			0x02
+#define RESP 		0x03
+#define RD 			0x04
+#define ERR 		0x05
+#define WTF			0xff
+
+struct ProtocolPacket
+{
+	unsigned int type :4;			//typ pakietu: RQ, RESP, RD, DATA, ACK, ERR
+	unsigned int str_length :8;		//dlugosc nazwy pliku (RQ, RD)
+	unsigned int number :20;		//numer (bloku danych (DATA) | potwierdzenia otrzymania bloku danych (ACK) | bledu (ERR) )
+	unsigned int data_size :32;		//rozmiar (bloku danych w bajtach (DATA) | calkowity rozmiar pliku (RESP) )
+	char* filename;					//dla RQ, RD
+	void* data;						//dla DATA
+};
+
 class ProtocolHandler
 {
-private:
+
 public:
 	ProtocolHandler();
+	struct ProtocolPacket prepareDatagramACK(unsigned int ack_number);
+	struct ProtocolPacket prepareDatagramRQ(unsigned int rq_str_length, const char* filename, unsigned int size_of_filename);
 	~ProtocolHandler();
 };
 
