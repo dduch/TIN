02dedb36f097b1f62db82323dfd5cd79ce9f798f
dduch <projecttin2015L@gmail.com>
2015-05-17 19:32:53 +0200

    kosmetyka

--- tin/src/Logger.cpp
+++ tin/src/Logger.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 
-//Konstruktor tworzy plik logu w folderze "log" aplikacji
+//Konstruktor tworzy plik logu w folderze "log" aplikacji - UWAGA - folder log musi istniec
 //Wynikowa nazwa pliku logu:
 //logfileName + logfileNumber(jeśli >= 0) + znacznik czasu
 Logger::Logger(const std::string& logfileName, int logfileNumber){
@@ -58,16 +58,11 @@ Logger:: ~Logger(){
 
 // Zapisuje wiadomosc do pliku logu w oddzielnej linii, poprzedzajac ja obecnym znacznikiem czasu
 // Dziala tylko gdy zezwolono na logowanie (domysnie logowanie jest odblokowane)
-<<<<<<< HEAD
 void Logger::logEvent(const std::string& logMessage, std::string type){
-=======
-void Logger::logEvent(const std::string& logMessage){
->>>>>>> 662ae1261bb4995e3bd47e4f013ebb588018d695
     if (loggingEnabled) {
         std::string msg;
         std::time_t timer;
         struct std::tm *timeinfo;
-<<<<<<< HEAD
 
         std::time (&timer);                 // Pobranie aktualnego czasu
         timeinfo = std::localtime(&timer);  // Konwersja czasu do lokalnego standardu
@@ -102,42 +97,7 @@ void Logger::enableLogging(){
     }
 }
 
-=======
-
-        std::time (&timer);                 // Pobranie aktualnego czasu
-        timeinfo = std::localtime(&timer);  // Konwersja czasu do lokalnego standardu
-
-        // Dopisanie znacznika czasu do wiadomosci
-        msg.append(std::to_string(timeinfo->tm_year + 1900));
-        msg.append("/");
-        msg.append(std::to_string(timeinfo->tm_mon + 1));
-        msg.append("/");
-        msg.append(std::to_string(timeinfo->tm_mday));
-        msg.append(" ");
-        msg.append(std::to_string(timeinfo->tm_hour));
-        msg.append(":");
-        msg.append(std::to_string(timeinfo->tm_min));
-        msg.append(":");
-        msg.append(std::to_string(timeinfo->tm_sec));
-        msg.append(" ");
-
-        msg.append(logMessage);             // Dopisanie wiadomosci
-        msg.append("\n");                   // Dodanie znaku nowej linii
-
-        write(logfd, msg.c_str(), msg.length());    // Zapis wiadomosci
-    }
-}
-
-// Zezwol na logowanie
-void Logger::enableLogging(){
-    if (logfd >= 0) {
-        loggingEnabled = true;
-    }
-}
-
->>>>>>> 662ae1261bb4995e3bd47e4f013ebb588018d695
 // Zablokuj logowanie  - metoda logEvent nie bedzie zapsisywala komunikatow do pliku
 void Logger::disableLogging(){
     loggingEnabled = false;
 }
-
--- tin/src/Logger.h
+++ tin/src/Logger.h
@@ -10,9 +10,9 @@
 #include <sys/types.h>
 #include "MessagePrinter.h"
 
-<<<<<<< HEAD
-
-// Typowe komunikaty logowania zdarzen
+/*
+ * Typowe komunikaty logowania zdarzen
+*/
 const std::string START_SENDING = "Rozpoczęto wysyłanie pliku ";
 const std::string START_DOWNLOADING = "Rozpoczęto pobieranie pliku ";
 const std::string RECEIVED_ACK = "Odebrano potwierdzenie odebrania pakietu numer ";
@@ -21,13 +21,13 @@ const std::string RECEIVED_DATA = "Odebrano pakiet z danymi o numerze ";
 const std::string FINISH_RECEIVING = "Zakończono odbieranie pliku ";
 const std::string FINISH_SENDING = "Zakończono wysyłanie pliku ";
 
-// Typowe prefixy dla komunikatów
+/*
+ * Typowe prefixy kominikatów
+*/
 const std::string INFO = "INFO: ";
 const std::string WARNING = "INFO: ";
 const std::string ERROR = "ERROR: ";
 
-=======
->>>>>>> 662ae1261bb4995e3bd47e4f013ebb588018d695
 class Logger{
 
 private:
@@ -45,11 +45,7 @@ public:
 
     // Zapisuje wiadomosc do pliku logu w oddzielnej linii, poprzedzajac ja obecnym znacznikiem czasu
     // Dziala tylko gdy zezwolono na logowanie (domysnie logowanie jest odblokowane)
-<<<<<<< HEAD
     void logEvent(const std::string& logMessage, std::string type);
-=======
-    void logEvent(const std::string& logMessage);
->>>>>>> 662ae1261bb4995e3bd47e4f013ebb588018d695
 
     // Zezwol na logowanie
     void enableLogging();
@@ -58,4 +54,3 @@ public:
     void disableLogging();
 };
 #endif
-
