31bf03420d87ea6b316600020f93d814e3a695b0
vitalik <vx0@yandex.ru>
2015-05-25 10:22:17 +0200

    Test framework

---
+++ tin/test/tests.cpp
@@ -0,0 +1,14 @@
+#include "../src/ProtocolHandler.h"
+#include <iostream>
+
+int test_protocol_handler_01();
+
+int main()
+{
+	test_protocol_handler_01();
+	return 0;
+}
+
+int test_protocol_handler_01(){
+	std::cout<<"OK"<<std::endl;
+}
